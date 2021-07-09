eacb1950f33b10e9571553ac9666341db3cb725b
Lochan Potdar <66880876+lochanp@users.noreply.github.com>
2021-07-09 19:19:06 +0530

    final

--- script.js
+++ script.js
@@ -1,8 +1,3 @@
-// const openBot= () => {
-//     let iframe = $("#forChatbot");
-//     iframe.attr("src", iframe.data("src")); 
-// }
-
 $(document).ready(function() {
     $('.icon').click(function(event) {
         $('.chatbox').toggle('active')
